c2e22413b3e93e6ad262dd3e6e0b5cdfef3d0ca4
Cabre <cabre94@hotmail.com>
2020-11-03 23:13:21 -0300

    First commit

---
+++ Practica_5/Cabrera.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+date: 22-10-2020
+File: Cabrera.py
+Author : Facundo Martin Cabrera
+Email: cabre94@hotmail.com facundo.cabrera@ib.edu.ar
+GitHub: https://github.com/cabre94
+GitLab: https://gitlab.com/cabre94
+Description:
+"""
+
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.datasets import imdb
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, activations, regularizers
+from tensorflow.keras import losses, metrics, optimizers
+from tensorflow.keras.regularizers import l2
+
+import seaborn as snn
+snn.set(font_scale=1)
+snn.set_style("darkgrid", {"axes.facecolor": ".9"})
+
+#--------------------------------------
+#           Ejercicio 3
+#--------------------------------------
+
+
+
+if __name__ == "__main__":
+
+    pass
