1c4cf155e68a5b4bb1827c539daf6d26e28b23ed
Cabre <cabre94@hotmail.com>
2020-11-21 01:24:40 -0300

    Added gradient ascent

--- TP_final/AE_white-box.py
+++ TP_final/AE_white-box.py
@@ -25,39 +25,41 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 
+import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import backend as K
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications import vgg16, vgg19, inception_resnet_v2
+from tensorflow.keras.applications import inception_v3
 
 
 # Cargo una red, creo que voy a arrancar con la VGG16
 
 # model = keras.applications.VGG16()
-model = inception_resnet_v2.InceptionResNetV2()
+# model = inception_resnet_v2.InceptionResNetV2()
+model = inception_v3.InceptionV3()
 
 # imagen = image.load_img("Cerveza.jpg")
-img = image.load_img("Cerveza.jpg", target_size=(299,299))
+imagen = image.load_img("Cerveza.jpg", target_size=(299,299))
 # imagen_3 = image.load_img("Cerveza.jpg", target_size=(1024,1024))
 
-arr_img = image.img_to_array(img)
-# imagen_2_arr = keras.preprocessing.image.img_to_array(imagen_2)
-# imagen_3_arr = keras.preprocessing.image.img_to_array(imagen_3)
+arr_img = image.img_to_array(imagen)   # imagen como array
 
-# imagen_arr = imagen_arr.reshape( (-1,imagen_arr.shape[0], imagen_arr.shape[1], imagen_arr.shape[2]) )
-# imagen_2_arr = imagen_2_arr.reshape( (-1,imagen_2_arr.shape[0], imagen_2_arr.shape[1], imagen_2_arr.shape[2]) )
-# imagen_3_arr = imagen_3_arr.reshape( (-1,imagen_3_arr.shape[0], imagen_3_arr.shape[1], imagen_3_arr.shape[2]) )
 
 arr_img = np.expand_dims(arr_img, axis=0)
 
 # Preprocesamos la imagen
-arr_img = inception_resnet_v2.preprocess_input(arr_img)
+arr_img = inception_v3.preprocess_input(arr_img)
+
+tensor_img = tf.convert_to_tensor(arr_img, dtype=tf.float64)
+
 
 
 # Hacemos una prediccion
 y = model.predict(arr_img)
 
 # Con esto sacamos los 5 (pero se pueden pedir mas), valores mas probables
-inception_resnet_v2.decode_predictions(y)
+inception_v3.decode_predictions(y)
 # decode_predictions(y)
 
 """Aca empieza lo importante"""
@@ -65,3 +67,82 @@ inception_resnet_v2.decode_predictions(y)
 Input = model.layers[0].input
 Output = model.layers[-1].output
 
+target = 7  # Creo que es una referencia a un limon en V3
+
+# De la salida, nos interesa la componente correspondiente a nuestra imagen
+# loss = Output[0, target]
+# target_output = Output[0, target]
+target_output = Output[:, target]   #XXX chequear si cambia en algo esto
+
+# Creamos el gradiente pero no entre el error y los parametros, si no sobre la variable de entrada
+# gradient = K.gradients(loss, Input)[0]
+
+kk = tf.image.convert_image_dtype(arr_img, dtype=tf.float64)
+
+# with tf.GradientTape() as tape:
+#     tape.watch(Input)
+
+# # gradient = tape.gradient(loss, Input)
+# # gradient = tape.gradient(loss, arr_img)
+# # gradient = tape.gradient(loss, img)
+# gradient = tape.gradient(loss, kk)
+
+#########################################################
+feature_extractor = keras.Model(inputs=Input, outputs=target_output)
+
+def compute_loss(img):
+    score = feature_extractor(img)
+    return tf.reduce_mean(score)
+
+
+@tf.function
+def gradient_ascent_step(adversarial, perturbation, epsilon):
+    
+    with tf.GradientTape() as tape:
+        tape.watch(adversarial)
+        loss = compute_loss(adversarial)
+    
+    # Calculo el gradiente
+    grad = tape.gradient(loss, adversarial)
+
+    # import ipdb; ipdb.set_trace(context=15)  # XXX BREAKPOINT
+
+    # Limitamos cuanto puede variar la imagen
+    # grad = tf.clip_by_value(grad, clip_value_min=-1*epsilon, clip_value_max=epsilon)
+
+    adversarial += grad
+    perturbation += grad
+
+    return loss, adversarial, perturbation
+
+def initialize_img_wZeros(img):
+    zeros = tf.zeros_like(img, dtype=tf.float64)
+    return zeros
+
+def generate_adversarial_example(adversarial):
+
+    perturbation = initialize_img_wZeros(adversarial)
+
+    epsilon = 1
+    # epsilon = 0.01
+
+    loss = compute_loss(adversarial).numpy()
+
+    steps = 0
+
+    while loss < 0.95:
+
+        # import ipdb; ipdb.set_trace(context=15)  # XXX BREAKPOINT
+
+        loss, adversarial, perturbation = gradient_ascent_step(adversarial, perturbation, epsilon)
+
+        loss = loss.numpy()
+        steps += 1
+
+        # print("Step {} - loss: {:.3f}".format(steps, loss))
+        print("Step {} - loss: {}".format(steps, loss))
+    
+    return adversarial, perturbation
+
+
+adver, pert = generate_adversarial_example(tensor_img)
